88c8276018a7f240322d882b0b4cec4bb5981114
Yusuf Sahin <yusuf@yusufsahin.net>
2023-11-01 00:02:54 +0300

    Security has been implemented unversioned

---
+++ SF700Security/src/main/java/com/example/sf700security/Sf700SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.sf700security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf700SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sf700SecurityApplication.class, args);
+    }
+
+}
---
+++ SF700Security/src/test/java/com/example/sf700security/Sf700SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sf700security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sf700SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
