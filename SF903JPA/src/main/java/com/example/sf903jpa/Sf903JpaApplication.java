8474ecccae9e8763ac67e8919f2422f181409bc9
Yusuf Sahin <yusuf@yusufsahin.net>
2023-11-03 01:47:51 +0300

    note jpa unversioned

---
+++ SF903JPA/src/main/java/com/example/sf903jpa/Sf903JpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sf903jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf903JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sf903JpaApplication.class, args);
+    }
+
+}
---
+++ SF903JPA/src/test/java/com/example/sf903jpa/Sf903JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sf903jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sf903JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
